2c9c1d49eca65e1f3fc17a39054c80c24d50ec47
andybridger <69238442+andybridger@users.noreply.github.com>
2021-02-06 15:58:41 +1000

    Updated code

--- aig_style.R
+++ aig_style.R
@@ -1,10 +1,6 @@
 #' Add Ai Group theme to ggplot chart
 #' This function allows you to add the Ai Group theme to your ggplotgraphics.
 
-#' line <- ggplot(line_df, aes(x = year, y = lifeExp)) +
-#' geom_line(colour = "#007f7f", size = 1) +
-#' geom_hline(yintercept = 0, size = 1, colour="#333333") +
-#' aig_style()
 # function
 aig_style <- function() {
   font <- "Arial"
@@ -13,14 +9,17 @@ aig_style <- function() {
     #Text format:
     #This sets the font, size, type and colour of text for the chart's title
     plot.title = ggplot2::element_text(family=font,
-                                       size=24,
+                                       size=20,
                                        face="bold",
                                        color="#950020"),
     #This sets the font, size, type and colour of text for the chart's subtitle, as well as setting a margin between the title and the subtitle
     plot.subtitle = ggplot2::element_text(family=font,
-                                          size=22,
+                                          size=16,
                                           margin=ggplot2::margin(3,0,3,0)),
-    plot.caption = ggplot2::element_blank(),
+    plot.caption = ggplot2::element_text(family=font,
+                                          size=10,
+                                          hjust = 0.0
+                                          ),
     #This leaves the caption text element empty, because it is set elsewhere in the finalise plot function
     
     #Legend format
@@ -31,19 +30,18 @@ aig_style <- function() {
     #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
     axis.title = ggplot2::element_blank(),
     axis.text = ggplot2::element_text(family=font,
-                                      size=18,
+                                      size=16,
                                       color="#000000"),
     axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
-    axis.line.y = ggplot2::element_blank(),
     axis.ticks.y = ggplot2::element_blank(),
-    axis.line.x.bottom = ggplot2::element_line(colour = "#000000", size = 1, linetype = 1),
-    axis.ticks.x.bottom = ggplot2::element_line(colour = "#000000", size = 1),
-
-    
+    axis.line.y = ggplot2::element_blank(),
+    axis.line.x.bottom = ggplot2::element_line(color="#000000",size = 0.5, linetype = 1),
+    axis.ticks.x.bottom = ggplot2::element_line(color="#000000",size = 0.5),
+      
     #Grid lines
     #This removes all minor gridlines and adds major y gridlines. In many cases you will want to change this to remove y gridlines and add x gridlines. The cookbook shows you examples for doing so
     panel.grid.minor = ggplot2::element_blank(),
-    panel.grid.major.y = ggplot2::element_line(color="#cbcbcb"),
+    panel.grid.major.y = ggplot2::element_line(color="#dcdcdc"),
     panel.grid.major.x = ggplot2::element_blank(),
     
     #Blank background
@@ -52,8 +50,6 @@ aig_style <- function() {
     
     #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
     strip.background = ggplot2::element_rect(fill="white"),
-    strip.text = ggplot2::element_text(size  = 22,  hjust = 0)
+    strip.text = ggplot2::element_text(size  = 16,  hjust = 0)
   )
 }
-
-
